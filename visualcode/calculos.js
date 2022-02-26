53a60997d73d5f6bee9b6529c175c0b03749bfd1
nascimento-ale <nascimento_ale@hotmail.com>
2022-02-26 16:56:41 -0300

    atividades visual code

---
+++ visualcode/calculos.js
@@ -0,0 +1,7 @@
+//calculos
+var valorum = 50;
+var valordois = 25;
+var total;
+//var total = valorum * valordois;
+var total = valorum / valordois;
+console.log(total);
---
+++ visualcode/nome.js
@@ -0,0 +1,7 @@
+//console.log('Diogo');
+// primeiro programa 
+ var nome = 'Diogo';
+ var sobrenome = 'Ferreira';
+ var idade = 19;
+ var nomecompleto = nome + sobrenome+ idade
+ console.log(nomecompleto);
