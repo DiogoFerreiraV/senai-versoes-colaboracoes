2aac58c33f4b968cfe34575e31a0d72776c92bc3
DiogoFerreiraV <Diogoferreirav21@outlook.com>
2022-03-05 17:29:08 -0300

    atividade visualcode1

---
+++ Visualcode1/atividade.js
@@ -0,0 +1,10 @@
+//calculos
+var valorum = 10;
+var valordois = 20;
+var valortres = 30;
+var valorquatro = 2;
+var total;
+//var total = (valorum + valordois + valortres) * valorquatro;
+//total = valorum ** valorquatro;
+total = valorum % valorquatro;
+console.log(total);
