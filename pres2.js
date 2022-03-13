c6aaaeea8fb8bb8b222297188b699b6f6ab36f55
Diogo Ferreira <Diogoferreirav21@outlook.com>
2022-03-13 00:17:19 -0300

    atividade presencial 2

---
+++ pres2.js
@@ -0,0 +1,12 @@
+//contagem da lista de alunos
+const alunos =14;
+for(let x=0; x <= alunos; x++)
+if(x ==0){
+    console.log("número zero");
+}
+else if(x %2 ==0){
+    console.log("o número é par: " +x);
+}
+else{
+    console.log("o número é impar: " +x);
+}
